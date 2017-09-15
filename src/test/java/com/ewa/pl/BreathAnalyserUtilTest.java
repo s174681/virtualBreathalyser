274bd6b0207e3fcef891a156aaa9e8be6c83b9b6
Ewa <e.polaniak@gmail.com>
2017-09-16 01:58:24 +0200

    Unity cd

--- src/test/java/com/ewa/pl/BreathAnalyserUtilTest.java
+++ src/test/java/com/ewa/pl/BreathAnalyserUtilTest.java
@@ -4,12 +4,65 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import com.ewa.pl.utils.BreathAnalyserUtil;
+import com.ewa.pl.model.Breathalyser.Gender;
 
 public class BreathAnalyserUtilTest {
 
+    private static final double FLOATING_POINT_BOUNDARY = 0.01;
+    private static final double DEFAULT_MALE_VALUE = 2.447;
+    private static final double DEFAULT_FIMALE_VALUE = -2.097;
+    private static final int NEGATIVE_VALUE = -1;
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+
+
     @Test
-    public void testCase() {
-        int a = 1;
-        assertEquals(1, a);
+    public void countTotalBodyWaterMaleClassOfEqualityTest() {
+
+        //For MALE: 2.447 - (0.09156 * age) + (0.1074 * height) + (0.3362 * weight);
+        assertEquals(DEFAULT_MALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.MALE, 0, 0, 0), FLOATING_POINT_BOUNDARY);
+
+        //Age<0 should change for 0;
+        assertEquals(DEFAULT_MALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.MALE, NEGATIVE_VALUE, 0, 0), FLOATING_POINT_BOUNDARY);
+        assertEquals(DEFAULT_MALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.MALE, ZERO, 0, 0), FLOATING_POINT_BOUNDARY);
+        assertEquals(DEFAULT_MALE_VALUE - 0.09156, BreathAnalyserUtil.countTotalBodyWater(Gender.MALE, ONE, 0, 0), FLOATING_POINT_BOUNDARY);
+
+        //Height<0 should change for 0;
+        assertEquals(DEFAULT_MALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.MALE, 0, NEGATIVE_VALUE, 0), FLOATING_POINT_BOUNDARY);
+        assertEquals(DEFAULT_MALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.MALE, 0, ZERO, 0), FLOATING_POINT_BOUNDARY);
+        assertEquals(DEFAULT_MALE_VALUE + 0.1074, BreathAnalyserUtil.countTotalBodyWater(Gender.MALE, 0, ONE, 0), FLOATING_POINT_BOUNDARY);
+
+        //Weight<0 should change for 0;
+        assertEquals(DEFAULT_MALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.MALE, 0, 0, NEGATIVE_VALUE), FLOATING_POINT_BOUNDARY);
+        assertEquals(DEFAULT_MALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.MALE, 0, 0, ZERO), FLOATING_POINT_BOUNDARY);
+        assertEquals(DEFAULT_MALE_VALUE + 0.3362, BreathAnalyserUtil.countTotalBodyWater(Gender.MALE, 0, 0, ONE), FLOATING_POINT_BOUNDARY);
+
+    }
+
+    @Test
+    public void countTotalBodyWaterFimaleClassOfEqualityTest() {
+
+        //For FIMALE: (0.1069 * height) + (0.2466 * weight) - 2.097;
+        assertEquals(DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, 0, 0), FLOATING_POINT_BOUNDARY);
+
+        //Height<0 should change for 0;
+        assertEquals(DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, NEGATIVE_VALUE, 0), FLOATING_POINT_BOUNDARY);
+        assertEquals(DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, ZERO, 0), FLOATING_POINT_BOUNDARY);
+        assertEquals(0.1069 +DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, ONE, 0), FLOATING_POINT_BOUNDARY);
+
+        //Weight<0 should change for 0;
+        assertEquals(DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, 0, NEGATIVE_VALUE), FLOATING_POINT_BOUNDARY);
+        assertEquals(DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, 0, ZERO), FLOATING_POINT_BOUNDARY);
+        assertEquals(0.2466 + DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, 0, ONE), FLOATING_POINT_BOUNDARY);
+
+    }
+
+    @Test
+    public void howManyHoursToAlcoholVanishClassOfEqualityTest() {
+        assertEquals("For negative values should return 0", 0, BreathAnalyserUtil.howManyHoursToAlcoholVanish(-2));
+        assertEquals("For 0 should return 0", 0, BreathAnalyserUtil.howManyHoursToAlcoholVanish(0));
+        assertEquals("For 1 should return 6", 6, BreathAnalyserUtil.howManyHoursToAlcoholVanish(1));
+        assertEquals("For 1 should return 1/0,15~=6", 6, BreathAnalyserUtil.howManyHoursToAlcoholVanish(1));
     }
 }
