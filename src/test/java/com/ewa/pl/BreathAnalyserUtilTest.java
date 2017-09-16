88fda00f015b2dba473b221f3eaf0a91198b1300
Ewa <e.polaniak@gmail.com>
2017-09-16 02:57:29 +0200

    Unity cd

--- src/test/java/com/ewa/pl/BreathAnalyserUtilTest.java
+++ src/test/java/com/ewa/pl/BreathAnalyserUtilTest.java
@@ -18,6 +18,37 @@ public class BreathAnalyserUtilTest {
 
 
     @Test
+    public void countConcentrationOfAlcoholClassOfEqualityTest() {
+
+        //(AlcoholMassInGrams / TotalBodyWater) * 0.8 - (TimeInHours * 0.15); Minimal TotalBodyWater is 1.0
+        // TotalBodyWater
+        assertEquals(ZERO, BreathAnalyserUtil.countConcentrationOfAlcohol(NEGATIVE_VALUE,0.0,0.0) , FLOATING_POINT_BOUNDARY);
+        assertEquals(0.8, BreathAnalyserUtil.countConcentrationOfAlcohol(ZERO,ONE,0.0) , FLOATING_POINT_BOUNDARY);
+        assertEquals(0.8, BreathAnalyserUtil.countConcentrationOfAlcohol(ONE,ONE,0.0) , FLOATING_POINT_BOUNDARY);
+
+        // AlcoholMassInGrams
+        assertEquals(ZERO, BreathAnalyserUtil.countConcentrationOfAlcohol(ONE,NEGATIVE_VALUE,0.0) , FLOATING_POINT_BOUNDARY);
+        assertEquals(ZERO, BreathAnalyserUtil.countConcentrationOfAlcohol(ONE,ZERO,0.0) , FLOATING_POINT_BOUNDARY);
+
+        // TimeInHours
+        assertEquals(ZERO, BreathAnalyserUtil.countConcentrationOfAlcohol(0.0,0.0,NEGATIVE_VALUE) , FLOATING_POINT_BOUNDARY);
+        assertEquals(ZERO, BreathAnalyserUtil.countConcentrationOfAlcohol(0.0,0.0,ZERO) , FLOATING_POINT_BOUNDARY);
+        assertEquals(-0.15, BreathAnalyserUtil.countConcentrationOfAlcohol(0.0,0.0,ONE) , FLOATING_POINT_BOUNDARY);
+
+    }
+
+    @Test
+    public void countAlcoholMassInGramsClassOfEqualityTest() {
+
+        //Percent * 0.01 * Quantity * Type * 0.79;
+        assertEquals(ZERO, BreathAnalyserUtil.countAlcoholMassInGrams(NEGATIVE_VALUE, 1, 1), FLOATING_POINT_BOUNDARY);
+        assertEquals(ZERO, BreathAnalyserUtil.countAlcoholMassInGrams(1, NEGATIVE_VALUE, 1), FLOATING_POINT_BOUNDARY);
+        assertEquals(ZERO, BreathAnalyserUtil.countAlcoholMassInGrams(1, 1, NEGATIVE_VALUE), FLOATING_POINT_BOUNDARY);
+        assertEquals(0.01 * 0.79, BreathAnalyserUtil.countAlcoholMassInGrams(1, 1, 1), FLOATING_POINT_BOUNDARY);
+
+    }
+
+    @Test
     public void countTotalBodyWaterMaleClassOfEqualityTest() {
 
         //For MALE: 2.447 - (0.09156 * age) + (0.1074 * height) + (0.3362 * weight);
@@ -49,7 +80,7 @@ public class BreathAnalyserUtilTest {
         //Height<0 should change for 0;
         assertEquals(DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, NEGATIVE_VALUE, 0), FLOATING_POINT_BOUNDARY);
         assertEquals(DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, ZERO, 0), FLOATING_POINT_BOUNDARY);
-        assertEquals(0.1069 +DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, ONE, 0), FLOATING_POINT_BOUNDARY);
+        assertEquals(0.1069 + DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, ONE, 0), FLOATING_POINT_BOUNDARY);
 
         //Weight<0 should change for 0;
         assertEquals(DEFAULT_FIMALE_VALUE, BreathAnalyserUtil.countTotalBodyWater(Gender.FIMALE, 0, 0, NEGATIVE_VALUE), FLOATING_POINT_BOUNDARY);
